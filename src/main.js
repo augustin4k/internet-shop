1ef5a39622b1b967570d39d818cb53e7aad3bfe7
augustin4k <cojocaru498@gmail.com>
2023-10-18 00:05:33 +0300

    solve some troubles with bootstrap

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 // main.js/ts
-import App from './App.vue' // Can be a different place
+import App from './App.vue'; // Can be a different place
 
-import {createApp} from 'vue'
-import BootstrapVueNext from 'bootstrap-vue-next'
+import { createApp } from 'vue';
+import BootstrapVueNext from 'bootstrap-vue-next';
 
 // Optional, since every component imports their Bootstrap functionality
 // the following line is not necessary
 // import 'bootstrap'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 
-const app = createApp(App)
-app.use(BootstrapVueNext)
-app.mount('#app')
+const app = createApp(App);
+app.use(BootstrapVueNext);
+app.mount('#app');
